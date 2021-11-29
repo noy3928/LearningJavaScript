e0511baa6c7c505bf71acb861c3ee615a5509f43
yechan <noy3928@naver.com>
2021-11-30 00:16:23 +0900

    파일을 시작하는 방법 canvas-sketch sketch.js canvas-sketch 이렇게 적은 다음에 열고자하는 파일명을 넣으면, 해당 파일을 시작시킨다. 새로운 파일을 만들고 싶다면, canvas-sketch sketch03.js --new --open 이런 식으로 적으면 된다.

--- Animation-domestika/sketches/sketch-02.js
+++ Animation-domestika/sketches/sketch-02.js
@@ -8,6 +8,10 @@ const degToRad = (degrees) => {
   return degrees / 180 * Math.PI;
 }
 
+const randomRange = (min, max) => {
+  return Math.random() * (max - min) + min;
+}
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -21,7 +25,7 @@ const sketch = () => {
     const h = height * 0.1;
     let x, y;
 
-    const num = 120;
+    const num = 12;
     const radius = width * 0.3;
 
 
@@ -36,6 +40,7 @@ const sketch = () => {
       context.save();
       context.translate(x,y)
       context.rotate(-angle)
+      context.scale(randomRange(1,3),1)
   
       context.beginPath();
       context.rect(-w * 0.5, -h * 0.5, w, h);
