2a1847341f093b685cc1a5c8b46896e4adaf2e9c
yechan <noy3928@naver.com>
2021-12-03 21:58:37 +0900

    클래스를 활용해 canvas 생성하기

--- Animation-domestika/sketches/sketch-03.js
+++ Animation-domestika/sketches/sketch-03.js
@@ -9,16 +9,24 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const point = {x : 800 , y : 400 , radius : 10};
+    const point = new Point(800, 400, 10);
 
     context.beginPath()
     context.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
     context.fillStyle = 'black';
     context.fill();
 
-    
+
 
   };
 };
 
 canvasSketch(sketch, settings);
+
+class Point {
+  constructor(x, y, radius){
+    this.x = x
+    this.y = y
+    this.radius = radius
+  }
+}
