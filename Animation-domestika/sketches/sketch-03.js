c42fe32fbcb6e858a5ddab7f468f0308c7f0ebbd
yechan <noy3928@naver.com>
2021-12-03 22:23:38 +0900

    canvas에 애니메이션을 입히기

--- Animation-domestika/sketches/sketch-03.js
+++ Animation-domestika/sketches/sketch-03.js
@@ -2,7 +2,8 @@ const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random')
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
 
 const sketch = ({ context, width, height }) => {
@@ -20,13 +21,16 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    agents.forEach(agent => agent.draw(context))
+    agents.forEach(agent => {
+      agent.update()
+      agent.draw(context)
+    })
   };
 };
 
 canvasSketch(sketch, settings);
 
-class Point {
+class Vector {
   constructor(x, y){
     this.x = x
     this.y = y
@@ -35,8 +39,14 @@ class Point {
 
 class Agent {
   constructor(x, y) {
-  this.pos = new Point(x, y);
+  this.pos = new Vector(x, y);
+  this.vel = new Vector(random.range(-1, 1), random.range(-1, 1))
   this.radius = random.range(4,12);
+  }
+
+  update(){
+    this.pos.x += this.vel.x
+    this.pos.y += this.vel.y 
   }
 
   draw(context){
