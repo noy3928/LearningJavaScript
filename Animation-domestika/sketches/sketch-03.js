a247cd2e098de7e6727598c262a4e71197042012
yechan <noy3928@naver.com>
2021-12-03 21:56:55 +0900

    객체를 활용해 canvas를 그리기

---
+++ Animation-domestika/sketches/sketch-03.js
@@ -0,0 +1,24 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    const point = {x : 800 , y : 400 , radius : 10};
+
+    context.beginPath()
+    context.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
+    context.fillStyle = 'black';
+    context.fill();
+
+    
+
+  };
+};
+
+canvasSketch(sketch, settings);
