cfeac9b1391a2607341c68b0be4fa2c0700315ac
yechan <noy3928@naver.com>
2021-12-03 22:04:48 +0900

    클래스에 메서드를 만들어 canvas에 그리기

--- Animation-domestika/sketches/sketch-03.js
+++ Animation-domestika/sketches/sketch-03.js
@@ -9,14 +9,11 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const point = new Point(800, 400, 10);
+    const agentA = new Agent(800, 400);
+    const agentB = new Agent(400, 800);
 
-    context.beginPath()
-    context.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
-    context.fillStyle = 'black';
-    context.fill();
-
-
+    agentA.draw(context);
+    agentB.draw(context);
 
   };
 };
@@ -24,9 +21,23 @@ const sketch = () => {
 canvasSketch(sketch, settings);
 
 class Point {
-  constructor(x, y, radius){
+  constructor(x, y){
     this.x = x
     this.y = y
-    this.radius = radius
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+  this.pos = new Point(x, y);
+  this.radius = 10;
+  }
+
+  draw(context){
+    context.fillStyle = 'black';
+
+    context.beginPath()
+    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.fill();
   }
 }
