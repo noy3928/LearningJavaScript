6815f64cdef1e89e150f96d5bf2e1736198dae8d
yechan <noy3928@naver.com>
2021-12-04 23:26:31 +0900

    캔버스 사이를 선으로 연결하기

--- Animation-domestika/sketches/sketch-03.js
+++ Animation-domestika/sketches/sketch-03.js
@@ -21,6 +21,19 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
+    for( let i = 0; i < agents.length; i ++){
+      const agent = agents[i];
+
+      for(let j = 0; j < agents.length; j ++){
+        const other = agents[j];
+
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+      }
+    }
+
     agents.forEach(agent => {
       agent.update()
       agent.draw(context)
