5938b1853cf8369e9d9415725e7f1a3972ef79e3
yechan <noy3928@naver.com>
2021-12-05 17:24:08 +0900

    setting course 4

---
+++ Animation-domestika/sketches/sketch-04.js
@@ -0,0 +1,34 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    const cols = 4;
+    const rows = 3;
+    const numCells = cols * rows;
+
+    const gridw = width * 0.8;
+    const gridh = height * 0.8;
+    const cellw = gridw / cols;
+    const cellh = gridh / rows;
+    const margx = (width - gridw) * 0.5;
+    const margy = (height - gridh) * 0.5;
+
+    for (let i = 0; i < numCells; i ++){
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      const x = col * cellw;
+      const y = row * cellh;
+      const w = cellw;
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
