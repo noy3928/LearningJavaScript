9413b5e585b4845cb291cb4f5c6f7233beeeeb5d
yechan <noy3928@naver.com>
2021-12-06 22:58:44 +0900

    병렬적인 라인 그리기

--- Animation-domestika/sketches/sketch-04.js
+++ Animation-domestika/sketches/sketch-04.js
@@ -9,8 +9,8 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const cols = 4;
-    const rows = 3;
+    const cols = 10;
+    const rows = 10;
     const numCells = cols * rows;
 
     const gridw = width * 0.8;
@@ -26,7 +26,22 @@ const sketch = () => {
 
       const x = col * cellw;
       const y = row * cellh;
-      const w = cellw;
+      const w = cellw * 0.8;
+      const h = cellh * 0.8;
+
+      context.save();
+      context.translate(x, y);
+      context.translate(margx, margy);
+      context.translate(cellw * 0.5, cellh * 0.5);
+
+      context.lineWidth = 4;
+
+      context.beginPath();
+      context.moveTo(w * -0.5, 0);
+      context.lineTo(w * 0.5, 0);
+      context.stroke();
+
+      context.restore();
     }
   };
 };
