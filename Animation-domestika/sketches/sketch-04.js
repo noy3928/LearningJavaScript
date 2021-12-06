30e15c5b3e851128960e378de8be1024420b5215
yechan <noy3928@naver.com>
2021-12-06 23:17:05 +0900

    랜덤한 선들에 noise한 스케일을 주고, noise한 애니메이션 주기

--- Animation-domestika/sketches/sketch-04.js
+++ Animation-domestika/sketches/sketch-04.js
@@ -1,11 +1,14 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
@@ -29,12 +32,20 @@ const sketch = () => {
       const w = cellw * 0.8;
       const h = cellh * 0.8;
 
+      const n = random.noise2D(x + frame * 10, y, 0.001);
+      const angle = n * Math.PI * 0.2;
+
+      // const scale = (n + 1) / 2 * 30;
+      // const scale = (n * 0.5 + 0.5) * 30;
+      const scale = math.mapRange(n, -1, 1, 1, 30);
+
       context.save();
       context.translate(x, y);
       context.translate(margx, margy);
       context.translate(cellw * 0.5, cellh * 0.5);
+      context.rotate(angle);
 
-      context.lineWidth = 4;
+      context.lineWidth = scale;
 
       context.beginPath();
       context.moveTo(w * -0.5, 0);
