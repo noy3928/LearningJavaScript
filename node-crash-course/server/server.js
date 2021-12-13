ae0f1a370375a7c2a23bd61bbd970f719464934e
yechan <noy3928@naver.com>
2021-12-14 00:10:23 +0900

    브라우저에게 응답으로 text를 보내기

--- node-crash-course/server/server.js
+++ node-crash-course/server/server.js
@@ -3,6 +3,12 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log('request made'); // 이것은 클라이언트 측에서는 나오지 않는다. 
     console.log(req.url, req.method) // 리퀘스트 객체 받아보기. 여기서 url/에 추가된 내용을 우리는 받아볼 수 있을 것이다. 
+
+    //set header content type, 브라우저에게 되돌려 줄 것을 정의하는 것. 
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.write('hello, ninjas');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
