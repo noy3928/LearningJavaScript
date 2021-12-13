fefa4fc02ab7b6685d07401623099b774b09d8b8
yechan <noy3928@naver.com>
2021-12-14 00:05:57 +0900

    request object에 대하여

--- node-crash-course/server/server.js
+++ node-crash-course/server/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log('request made'); // 이것은 클라이언트 측에서는 나오지 않는다. 
+    console.log(req.url, req.method) // 리퀘스트 객체 받아보기. 여기서 url/에 추가된 내용을 우리는 받아볼 수 있을 것이다. 
 });
 
 server.listen(3000, 'localhost', () => {
