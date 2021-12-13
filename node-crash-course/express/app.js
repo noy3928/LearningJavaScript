68a6c7ed126ae106ce6acae11a8a7db515e93ca5
yechan <noy3928@naver.com>
2021-12-14 02:49:19 +0900

    express routing and redirect

--- node-crash-course/express/app.js
+++ node-crash-course/express/app.js
@@ -7,7 +7,15 @@ const app = express()
 app.listen(3000);
 
 app.get('/', (req, res) => {
+    // res.send('<p>home page</p>'); // express를 이용하면, header 설정을 자동으로 해주기 때문에 따로 헤더를 설정해주지 않아도 된다. 
+    res.sendFile('./views/index.html', {root: __dirname}); // 파일을 보내주는 방법
+})
 
-res.send('<p>home page</p>'); // express를 이용하면, header 설정을 자동으로 해주기 때문에 따로 헤더를 설정해주지 않아도 된다. 
+app.get('/about', (req, res) => {
+    // res.send('<p>about page</p>'); // express를 이용하면, header 설정을 자동으로 해주기 때문에 따로 헤더를 설정해주지 않아도 된다. 
+    res.sendFile('./views/about.html', {root: __dirname}); // 파일을 보내주는 방법
+})
 
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
 })
