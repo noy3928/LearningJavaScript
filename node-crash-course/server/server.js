43b32f76340723b17b78bea60be6d608c006efb6
yechan <noy3928@naver.com>
2021-12-14 02:30:55 +0900

    install npm and some library

--- node-crash-course/server/server.js
+++ node-crash-course/server/server.js
@@ -1,9 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log('request made'); // 이것은 클라이언트 측에서는 나오지 않는다. 
-    console.log(req.url, req.method) // 리퀘스트 객체 받아보기. 여기서 url/에 추가된 내용을 우리는 받아볼 수 있을 것이다. 
+    // console.log('request made'); // 이것은 클라이언트 측에서는 나오지 않는다. 
+    // console.log(req.url, req.method) // 리퀘스트 객체 받아보기. 여기서 url/에 추가된 내용을 우리는 받아볼 수 있을 것이다. 
+
+    const num = _.random(0, 20);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log('hello');
+    })
+
+    greet()
+    greet()
 
     //set header content type, 브라우저에게 되돌려 줄 것을 정의하는 것. 
     res.setHeader('Content-Type', 'text/html');
