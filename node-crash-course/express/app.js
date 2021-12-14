8b11f3ea19f93f6652ebe6f845a22f2bbf9cdc71
yechan <noy3928@naver.com>
2021-12-14 23:06:32 +0900

    공용 ejs 파일을 만들어서 사용하기
    사용해보니 리액트의 컴포넌트와 개념이 비슷한듯 하다

--- node-crash-course/express/app.js
+++ node-crash-course/express/app.js
@@ -12,9 +12,9 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
     const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        // {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        // {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        // {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
       ];
     res.render('index', {title: 'Home', blogs})
 })
