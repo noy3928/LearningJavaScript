03c5d14f9aff639d7320f919d5318b6a0e2cb847
yechan <noy3928@naver.com>
2021-12-14 22:53:17 +0900

    using ejs

--- node-crash-course/express/app.js
+++ node-crash-course/express/app.js
@@ -3,27 +3,34 @@ const express = require('express')
 //exporess app
 const app = express()
 
+//register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<p>home page</p>'); // express를 이용하면, header 설정을 자동으로 해주기 때문에 따로 헤더를 설정해주지 않아도 된다. 
-    res.sendFile('./views/index.html', {root: __dirname}); // 파일을 보내주는 방법
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    res.render('index', {title: 'Home', blogs})
 })
 
 app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>'); // express를 이용하면, header 설정을 자동으로 해주기 때문에 따로 헤더를 설정해주지 않아도 된다. 
-    res.sendFile('./views/about.html', {root: __dirname}); // 파일을 보내주는 방법
+    res.render('about', {title: 'About'})
 })
 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create a new Blog'})
 })
 
 //404 page
 app.use((req, res) => {
     //use에서는 매치되지 않는 url에 대해서 반응한다. 
-    res.sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404', {title : '404'})
 })
 
 
