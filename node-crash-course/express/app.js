adb07ed9d902cffb7cb22c393ba1d82506cdb5ea
yechan <noy3928@naver.com>
2021-12-18 13:58:03 +0900

    feat : sorting the data

--- node-crash-course/express/app.js
+++ node-crash-course/express/app.js
@@ -79,7 +79,7 @@ app.get('/about', (req, res) => {
 
 // blog routes
 app.get('/blogs', (req, res) => {
-    Blog.find()
+    Blog.find().sort({ createAt: -1 })
         .then((result) => {
             res.render('index', {title : 'All Blogs', blogs: result })
         })
