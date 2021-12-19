336a8eb19d4742cd5cac85d4c8474ae7c4726afe
yechan <noy3928@naver.com>
2021-12-19 11:09:15 +0900

    feat : make get request

--- node-crash-course/express/app.js
+++ node-crash-course/express/app.js
@@ -21,6 +21,7 @@ app.set('view engine', 'ejs');
 
 //middleware & static files
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'))
 
 // //mongoose and mongo sandbox routes
@@ -79,13 +80,25 @@ app.get('/about', (req, res) => {
 
 // blog routes
 app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createAt: -1 })
+    Blog.find().sort({ createdAt: -1 })
         .then((result) => {
             res.render('index', {title : 'All Blogs', blogs: result })
-        })
+        }) 
         .catch((err) => {
             console.log(err);
         })
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+    .then((result) => {
+        res.redirect('/blogs');
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 })
 
 app.get('/blogs/create', (req, res) => {
