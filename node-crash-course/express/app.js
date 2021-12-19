99a5b6ac634f790268c2b2522181c08d8dc95692
yechan <noy3928@naver.com>
2021-12-19 23:52:18 +0900

    feat : seperate route to other folder

--- node-crash-course/express/app.js
+++ node-crash-course/express/app.js
@@ -79,56 +79,6 @@ app.get('/about', (req, res) => {
     res.render('about', {title: 'About'})
 })
 
-// blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', {title : 'All Blogs', blogs: result })
-        }) 
-        .catch((err) => {
-            console.log(err);
-        })
-})
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-    .then((result) => {
-        res.redirect('/blogs');
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-})
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-    .then(result => {
-        res.render('detail', { blog: result, title: 'Blog Details' })
-    })
-    .catch(err => {
-        console.log(err);
-    })
-})
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-    .then(result => {
-        res.json({redirect: '/blogs'})
-    })
-    .catch(err => {
-        console.log(err)
-    })
-})
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create a new Blog'})
-})
-
 //404 page
 app.use((req, res) => {
     //use에서는 매치되지 않는 url에 대해서 반응한다. 
