089832656200a78cc7b1aa9ba4235aa77b9b803c
yechan <noy3928@naver.com>
2021-12-22 00:03:40 +0900

    add index.ts and make api call func

---
+++ SDK/index.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_URL: string = 'https://pokeapi.co/api/v2';
+
+export function getPokemonById(id: number): Promise<object>{
+    return new Promise((resolve, reject) => {
+        axios
+        .get(`${API_URL}/pokemon/${id}`)
+        .then((resp) => {
+            resolve(resp.data);
+        })
+        .catch(reject);
+    })
+}
+
+export default { getPokemonById };
