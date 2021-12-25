7ed29897522df925cd1a42e6f33452a95d507b59
yechan <noy3928@naver.com>
2021-12-25 23:43:14 +0900

    make Cat class using extends keyword with Dog

--- 자바스크립트 심화(애플코딩)/클래스/class-prac1.js
+++ 자바스크립트 심화(애플코딩)/클래스/class-prac1.js
@@ -8,3 +8,11 @@ class Dog {
 const 강아지1 = new Dog('똥개', 'black');
 
 
+class Cat extends Dog{
+    constructor(type, color, age){
+        super(type, color);
+        this.age = age;
+    }
+}
+
+const 고양이1 = new Cat('러시안블루', 'white', 2);
