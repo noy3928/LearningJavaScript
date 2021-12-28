ef7a4b1bb9908731e644de033be074f5862ef084
yechan <noy3928@naver.com>
2021-12-28 11:52:48 +0900

    add mongoose

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000; //포트는 아무거나 해도 상관없다. 
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://noy3928:sdc03928@boilerplate.6rnsp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
