561c3ec259ffdd14950ebf0d1592d4d2f76acfc2
yechan <noy3928@naver.com>
2022-02-13 17:43:18 +0900

    mern-project : make user register validation

--- Courses/mern-project/routes/api/users.js
+++ Courses/mern-project/routes/api/users.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check')
 
-// @route  GET api/users
+// @route  POST api/users
 // @desc   Test route
 // @access Public  => 토큰이 필요한지 아닌지 
-router.get('/', (req, res) => res.send('User route'));
+router.post('/',[
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+] ,(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    console.log(req.body);
+    res.send('User route')
+});
 
 module.exports = router; 
 
