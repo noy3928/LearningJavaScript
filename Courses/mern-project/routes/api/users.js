78098f0053cf60bc5300b27b98e3b70d82788485
yechan <noy3928@naver.com>
2022-02-14 00:28:21 +0900

    mern-project : implementing JWT

--- Courses/mern-project/routes/api/users.js
+++ Courses/mern-project/routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
@@ -52,7 +54,23 @@ router.post('/',[
     await user.save();
 
     // Return jsonwebtoken 
-    res.send('User route')
+    // res.send('User route')
+
+    const payload = {
+        user : {
+            id : user.id
+        }
+    }
+
+    jwt.sign(
+        payload, 
+        config.get('jwtSecret'), 
+        { expiresIn: 36000 }, 
+        (err, token) => {
+        if(err) throw err;
+        res.json({ token });
+    }
+    );
 
     } catch (err) {
         console.error(err.message);
