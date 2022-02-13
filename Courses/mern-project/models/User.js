eed8b941e461b820a2dd99de7bfc075d0b8b7ae4
yechan <noy3928@naver.com>
2022-02-13 16:11:20 +0900

    mern-project : make UserSchema

---
+++ Courses/mern-project/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique: true
+    },
+    password :{
+        type : String,
+        required: true
+    },
+    avatar : {
+        type : String
+    },
+    date : {
+        type : Data,
+        default: Data.now
+    }
+})
+
+module.exports = User = mongoose.model('user', UserSchema)
