9afb801b81f22c91f471d585b61a3888dd4cf6d1
yechan <noy3928@naver.com>
2022-02-15 00:26:49 +0900

    mern-project : JWT verify

--- Courses/mern-project/routes/api/auth.js
+++ Courses/mern-project/routes/api/auth.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth')
+
+const User = require('../../models/User');
 
 // @route  GET api/auth
 // @desc   Test route
 // @access Public  => 토큰이 필요한지 아닌지 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+        console.log(user)
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+});
 
 module.exports = router; 
 
