8ec2e8e0bf73c01d5fd326ec347466ea7244fa0d
yechan <noy3928@naver.com>
2022-02-17 00:06:10 +0900

    mern-project : get profiles

--- Courses/mern-project/routes/api/profile.js
+++ Courses/mern-project/routes/api/profile.js
@@ -101,5 +101,38 @@ async (req, res) => {
     }
 })
 
+// @route  GET api/profile
+// @desc   GET all profiles
+// @access Public  => 토큰이 필요한지 아닌지 
+router.get('/', async (req, res) => {
+    try {
+        const profile = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profile);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route  GET api/profile/user/:user_id
+// @desc   GET profile by user ID
+// @access Public  => 토큰이 필요한지 아닌지 
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user : req.params.user_id }).populate('user', ['name', 'avatar']);
+
+        if(!profile) return res.status(400).json({ msg : 'Profile not found'})
+
+        res.json(profile);
+    } catch(err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId'){
+            return res.status(400).json({ msg : 'Profile not found'})
+
+        }
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router; 
 
