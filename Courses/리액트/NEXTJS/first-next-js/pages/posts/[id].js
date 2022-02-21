e3ed90c8b4f6e4604b15e1648830ca75aadebb79
yechan <noy3928@naver.com>
2022-02-22 00:32:14 +0900

    Typescript : union and intersaction

--- Courses/리액트/NEXTJS/first-next-js/pages/posts/[id].js
+++ Courses/리액트/NEXTJS/first-next-js/pages/posts/[id].js
@@ -2,16 +2,18 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
-    return (
-      <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  )
+}
 
 // getStaticPaths which returns an array of possible values for id 
 export async function getStaticPaths() {
