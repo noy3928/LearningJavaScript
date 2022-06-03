6fc96176f5d578c2233f378cb2e348a8016a3471
yechan <noy3928@naver.com>
2022-06-03 23:17:32 +0900

    puppeteer tutorial

---
+++ etc/Puppeteer/ycombinator-scraper.js
@@ -0,0 +1,42 @@
+const puppeteer = require("puppeteer")
+function run(pagesToScrape) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!pagesToScrape) {
+        pagesToScrape = 1
+      }
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.goto("https://news.ycombinator.com/")
+      let currentPage = 1
+      let urls = []
+      while (currentPage <= pagesToScrape) {
+        let newUrls = await page.evaluate(() => {
+          let results = []
+          let items = document.querySelectorAll("a.storylink")
+          items.forEach(item => {
+            results.push({
+              url: item.getAttribute("href"),
+              text: item.innerText,
+            })
+          })
+          return results
+        })
+        urls = urls.concat(newUrls)
+        if (currentPage < pagesToScrape) {
+          await Promise.all([
+            await page.click("a.morelink"),
+            await page.waitForSelector("a.storylink"),
+          ])
+        }
+        currentPage++
+      }
+
+      browser.close()
+      return resolve(urls)
+    } catch (e) {
+      return reject(e)
+    }
+  })
+}
+run().then(console.log).catch(console.error)
