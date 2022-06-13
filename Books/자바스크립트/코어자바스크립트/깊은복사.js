8e4fc4e83fbef1599e4617d2ed3a95d17d51222c
yechan <noy3928@naver.com>
2022-06-13 11:34:05 +0900

    JS : 깊은 복사

---
+++ Books/자바스크립트/코어자바스크립트/깊은복사.js
@@ -0,0 +1,21 @@
+var copyObjectDeep = function (target) {
+  var result = {}
+  if (typeof target === "object" && target !== null) {
+    for (var prop in target) {
+      result[prop] = copyObjectDeep(target[prop])
+    }
+  } else {
+    result = target
+  }
+  return result
+}
+
+var obj = {
+  a: 1,
+  b: {
+    c: null,
+    d: [1, 2],
+  },
+}
+
+var obj2 = copyObjectDeep(obj)
