28c1ff1449170646ff2f0a345244e6a01758b239
yechan <noy3928@naver.com>
2022-06-17 21:49:12 +0900

    JS : 깊은 복사 유틸 리팩토링

--- Books/자바스크립트/코어자바스크립트/깊은복사.js
+++ Books/자바스크립트/코어자바스크립트/깊은복사.js
@@ -15,7 +15,55 @@ var obj = {
   b: {
     c: null,
     d: [1, 2],
+    e: {
+      f: [1, 2, 3, 4],
+    },
   },
 }
 
 var obj2 = copyObjectDeep(obj)
+
+//----------------2 check 부분 분리하기
+
+const checkObj = target => {
+  if (typeof target === "object" && target !== null) return true
+  else return false
+}
+
+const copyObjectDeep2 = target => {
+  var result = {}
+  if (checkObj(target)) {
+    for (var prop in target) {
+      result[prop] = copyObjectDeep2(target[prop])
+    }
+  } else {
+    result = target
+  }
+  return result
+}
+
+const obj3 = copyObjectDeep2(obj)
+console.log(obj3)
+
+//----------3 object.assign으로 만들어보기
+
+const copyShallow = target => {
+  return Object.assign({}, target)
+}
+
+const copyObjectDeep3 = target => {
+  var result = {}
+  if (checkObj(target)) {
+    for (var prop in target) {
+      result[prop] = copyObjectDeep3(target[prop])
+    }
+  } else {
+    return copyShallow(target)
+  }
+  return result
+}
+
+const obj4 = copyObjectDeep3(obj)
+const obj5 = copyShallow(obj)
+
+console.log(obj5.b.e)
