6a246e4e19ee39a2d0b9f75a8de7e651a94555f2
yechan <noy3928@naver.com>
2022-07-01 08:32:13 +0900

    WC : Add Image Component

--- Question-Study/웹컴포넌트/motion/dist/app.js
+++ Question-Study/웹컴포넌트/motion/dist/app.js
@@ -1,10 +1,13 @@
-import { PageComponent } from "./components/page.js"
+import { PageComponent } from "./components/page/page.js";
+import { ImageComponent } from "./components/page/item/image.js";
 var App = (function () {
-  function App(appRoot) {
-    this.page = new PageComponent()
-    this.page.attachTo(appRoot)
-  }
-  return App
-})()
-export { App }
-new App(document.querySelector(".document"))
+    function App(appRoot) {
+        this.page = new PageComponent();
+        this.page.attachTo(appRoot);
+        var image = new ImageComponent("Image Title", "https://picsum.photos/600/300");
+        image.attachTo(appRoot, "beforeend");
+    }
+    return App;
+}());
+export { App };
+new App(document.querySelector(".document"));
