d545e96a4b0f4a6f9901cf0d8c242fd90a2836d5
yechan <noy3928@naver.com>
2022-07-03 23:25:48 +0900

    Implement Virtual Dom - createElement method

--- Question-Study/깊은복사/clone.test.js
+++ Question-Study/깊은복사/clone.test.js
@@ -20,49 +20,6 @@ describe("cloneDeep", () => {
     },
   }
 
-  var obj1 = {
-    a: 1,
-    b: {
-      c: null,
-      d: [1, 2],
-      e: {
-        f: [1, 2, 3, 4],
-      },
-      g: new Function(),
-      h: new Date(),
-      k: new Set([1, 2, 3, 4, { a: 1, b: 2 }]),
-      l: new Map(),
-      i: new RegExp(),
-      j: Symbol("a"),
-      m: 42 / +0,
-    },
-  }
-
-  const can1 = {
-    flavor: "grapefruit",
-    ounces: 12,
-    hi: {
-      d: 1,
-    },
-  }
-  const can2 = {
-    flavor: "grapefruit",
-    ounces: 12,
-    hi: {
-      d: 1,
-    },
-  }
-
-  test("have all the same properties", () => {
-    expect(can1).toEqual(can2)
-  })
-  test("have all the same properties", () => {
-    expect(can1.hi).toBe(can2.hi)
-  })
-  test("are not the exact same can", () => {
-    expect(can1).not.toBe(can2)
-  })
-
   function compareObject(target1, target2) {
     const isObject =
       typeof target1 === "object" &&
@@ -71,6 +28,7 @@ describe("cloneDeep", () => {
       target2 !== null
 
     if (isObject) {
+      console.log(target1, target2)
       expect(target1).not.toBe(target2)
       for (const prop in target1) {
         compareObject(target1[prop], target2[prop])
@@ -80,23 +38,19 @@ describe("cloneDeep", () => {
     }
   }
 
-  // it("should have value equality in object - toEqual", () => {
-  //   expect(obj).toEqual(obj1)
-  // })
+  it("should have value equality in object - toEqual", () => {
+    expect(obj).toEqual(cloneObjectDeep(obj))
+  })
 
-  // it("should have different reference - my util", () => {
-  //   compareObject(obj, cloneObjectDeep(obj))
-  // })
+  it("should have different reference - my util", () => {
+    compareObject(obj, cloneObjectDeep(obj))
+  })
 
-  // it("should have different reference - lodash", () => {
-  //   compareObject(obj, _.cloneDeep(obj))
-  // })
+  it("should have different reference - lodash", () => {
+    compareObject(obj, _.cloneDeep(obj))
+  })
 
-  // it("should have value equality in object - toEqual", () => {
-  //   expect(cloneObjectDeep(obj)).toEqual(obj)
-  // })
-
-  // it("should have different reference - not toStrictEqual", () => {
-  //   expect(cloneObjectDeep(obj)).toStrictEqual(obj)
-  // })
+  it("should have value equality in object - toEqual", () => {
+    expect(cloneObjectDeep(obj)).toEqual(obj)
+  })
 })
