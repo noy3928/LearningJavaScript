e7a04c659881db0e73ef88f2aa4a261b9c66dbe9
yechan <noy3928@naver.com>
2022-07-04 11:21:29 +0900

    make updateElement method for virtual dom implement

--- Question-Study/VirtualDom/virtualDom.js
+++ Question-Study/VirtualDom/virtualDom.js
@@ -20,3 +20,32 @@ const a = (
 
 const $root = document.getElementById("root")
 $root.appendChild(createElement(a))
+
+function changed(node1, node2) {
+  return (
+    typeof node1 !== typeof node2 ||
+    (typeof node1 === "string" && node1 !== node2) ||
+    node1.type !== node2.type
+  )
+}
+
+function updateElement($parent, newNode, oldNode, index = 0) {
+  if (!oldNode) {
+    $parent.appendChild(createElement(newNode))
+  } else if (!newNode) {
+    $parent.removeChild($parent.childNodes[index])
+  } else if (changed(newNode, oldNode)) {
+    $parent.replaceChild(createElement(newNode), $parent.childNodes[index])
+  } else if (newNode.type) {
+    const newLength = newNode.children.length
+    const oldLength = oldNode.children.length
+    for (let i = 0; i < newLength || i < oldLength; i++) {
+      updateElement(
+        $parent.childNodes[index],
+        newNode.children[i],
+        oldNode.children[i],
+        i
+      )
+    }
+  }
+}
