c65a3099156ababb16513f1b6ec7f11e0cd5a7d1
yechan <noy3928@naver.com>
2022-08-19 14:49:59 +0900

    implement router from scratch - prac7

---
+++ Question-Study/Router/prac7/router.js
@@ -0,0 +1,95 @@
+let Router = {
+  routes: [],
+  mode: null,
+  root: "/",
+  config: function (options) {
+    this.mode =
+      options &&
+      options.mode &&
+      options.mode == "history" &&
+      !!history.pushState
+        ? "history"
+        : "hash"
+    this.root =
+      options && options.root
+        ? "/" + this.clearSlashes(options.root) + "/"
+        : "/"
+    return this
+  },
+  getFragment: function () {
+    let fragment = ""
+    if (this.mode === "history") {
+      fragment = this.clearSlashes(
+        decodeURI(location.pathname + location.search)
+      )
+      fragment = fragment.replace(/\\?(.*)$/, "")
+      fragment = this.root != "/" ? fragment.replace(this.root, "") : fragment
+    } else {
+      let match = window.location.href.match(/#(.*)$/)
+      fragment = match ? match[1] : ""
+    }
+    return this.clearSlashes(fragment)
+  },
+  clearSlashes: function (path) {
+    return path.toString().replace(/\/$/, "").replace(/^\//, "")
+  },
+  add: function (re, handler) {
+    if (typeof re == "function") {
+      handler = re
+      re = ""
+    }
+    this.routes.push({ re, handler })
+    return this // this를 반환해주는 이유는 chain of method를 위해서.
+  },
+  remove: function (param) {
+    for (var i = 0, r; i < this.routes.length, (r = this.routes[i]); i++) {
+      if (r.handler === param || r.re.toString() === param.toString()) {
+        this.routes.splice(i, 1)
+        return this
+      }
+    }
+    return this
+  },
+  flush: function () {
+    this.routes = []
+    this.mode = null
+    this.root = "/"
+    return this
+  },
+  check: function (f) {
+    let fragment = f || this.getFragment()
+    for (let i = 0; i < this.routes.length; i++) {
+      let match = fragment.match(this.routes[i].re)
+      if (match) {
+        match.shift()
+        this.routes[i].handler.apply({}, match)
+        return this
+      }
+    }
+    return this
+  },
+  listen: function () {
+    var self = this
+    var current = self.getFragment()
+    var fn = function () {
+      if (current !== self.getFragment()) {
+        current = self.getFragment()
+        self.check(current)
+      }
+    }
+    clearInterval(this.interval)
+    this.interval = setInterval(fn, 50)
+    return this
+  },
+}
+
+Router.add(/about/, function () {
+  console.log("about")
+})
+  .add(/products\/(.*)\/edit\/(.*)/, function () {
+    console.log("products", arguments)
+  })
+  .add(function () {
+    console.log("default")
+  })
+  .check("/products/12/edit/22")
