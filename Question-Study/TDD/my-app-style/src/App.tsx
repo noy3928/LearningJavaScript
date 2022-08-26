b0c1b1691f1af6ae61f35803aa50f34faa2cce3a
yechan <noy3928@naver.com>
2022-08-26 16:00:11 +0900

    feat : add setToDolist function

--- Question-Study/TDD/my-app-style/src/App.tsx
+++ Question-Study/TDD/my-app-style/src/App.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import { Button, Input, ToDoItem } from 'Components';
 
 function App() {
+  const [toDo, setToDo] = useState('');
+  const [toDoList, setToDoList] = useState<string[]>([]);
+
+  const addToDo = (): void => {
+    if (toDo) {
+      setToDoList([...toDoList, toDo]);
+      setToDo('');
+    }
+  };
+
+  const deleteToDo = (index: number): void => {
+    let list = [...toDoList];
+    list.splice(index, 1);
+    setToDoList(list);
+  };
+
   return (
     <Container>
       <Contents>
-        <ToDoItem label="추가된 할 일" onDelete={() => alert('삭제')} />
+        <ToDoListContainer>
+          {toDoList.map((item, index) => (
+            <ToDoItem key={item} label={item} onDelete={() => deleteToDo(index)} />
+          ))}
+        </ToDoListContainer>
         <InputContainer>
-          <Input placeholder="할 일을 입력해 주세요" onChange={(text) => console.log(text)} />
-          <Button label="추가" onClick={() => alert('추가')} />
+          <Input placeholder="할 일을 입력해 주세요" onChange={(text) => setToDo(text)} />
+          <Button label="추가" onClick={() => alert(toDo)} />
         </InputContainer>
       </Contents>
     </Container>
@@ -40,4 +58,12 @@ const Contents = styled.div`
 
 const InputContainer = styled.div`
   display: flex;
+`;
+
+const ToDoListContainer = styled.div`
+  min-width: 350px;
+  height: 400px;
+  overflow-y: scroll;
+  border: 1px solid #bdbdbd;
+  margin-bottom: 20px;
 `;
