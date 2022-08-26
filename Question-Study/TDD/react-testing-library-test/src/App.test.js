c23cfdf38513dee6952e52ad01b4b73366b7cbc3
yechan <noy3928@naver.com>
2022-08-26 12:17:21 +0900

    using react-testing-library

---
+++ Question-Study/TDD/react-testing-library-test/src/App.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react"
+import App from "./App"
+
+test("renders learn react link", () => {
+  render(<App />)
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+describe("<App/>", () => {
+  it("renders component correctly", () => {
+    const { container } = render(<App />)
+    expect(container.getElementsByClassName("App-logo")).toHaveLength(1)
+    expect(container.getElementsByClassName("App-logo")[0]).toHaveAttribute(
+      "src",
+      "logo.svg"
+    )
+    expect(container.getElementsByTagName("p")).toHaveLength(1)
+    expect(container.getElementsByTagName("p")[0]).toHaveTextContent(
+      "Edit src/App.js and save to reload"
+    )
+
+    const linkElement = screen.getByText(/learn react/i)
+    expect(linkElement).toBeInTheDocument()
+
+    expect(container).toMatchSnapshot()
+  })
+})
